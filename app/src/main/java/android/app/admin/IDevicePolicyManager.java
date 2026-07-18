8bed390c9af2422993c29b368f6c2b83780d46bc
RaBU-W <bhanwarlalbangra7@gmail.com>
2026-07-18 09:59:44 +0530

    Fix private DNS host constant initialization

---
+++ app/src/main/java/android/app/admin/IDevicePolicyManager.java
@@ -0,0 +1,19 @@
+package android.app.admin;
+
+import android.content.ComponentName;
+import android.os.Binder;
+import android.os.IBinder;
+import android.os.IInterface;
+import androidx.annotation.Keep;
+
+@Keep
+public interface IDevicePolicyManager extends IInterface {
+    @Keep
+    abstract class Stub extends Binder implements IDevicePolicyManager {
+        public static IDevicePolicyManager asInterface(IBinder obj) {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    int setGlobalPrivateDns(ComponentName who, int mode, String privateDnsHost);
+}
